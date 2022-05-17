e50950211640eb609974514805cda864517adad0
Richovrnndo <Richovrnando@gmail.com>
2022-05-17 09:33:23 +0700

    update

---
+++ variable-const.js
@@ -0,0 +1,3 @@
+const application = "application";
+
+document.writeln(application);
---
+++ variable.js
@@ -0,0 +1,8 @@
+let fullName = "Richo vernando" + " test";
+let middleName = "cho"; // nama variable harus camelCase
+let keterangan = "let";
+
+document.writeln(fullName);
+document.writeln(fullName);
+document.writeln(middleName);
+document.writeln(keterangan);
